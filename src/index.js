159ce0c032585ebea89be57fa5dae241e848c010
Xiaohan <xiaohan.zhu@outlook.com>
2018-06-05 17:30:11 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,2 @@
+/* 入口文件经过处理后，生成 bundle.js */
+document.getElementById('app').innerHTML = "Webpack works"
---
+++ webpack.dev.config.js
@@ -0,0 +1,13 @@
+/* webpack 开发配置文件 */
+const path = require('path');
+
+module.exports = {
+    /* 入口 */
+    entry : path.join(__dirname, 'src/index.js'),
+
+    /* 输出到dist文件夹，输出名字为bundle.js */
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js'
+    }
+};
